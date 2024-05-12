dc058ef76bb0102459d234b5c556b67eafe131d2
alexandre-longeaud <alexandre.longeaud08@gmail.com>
2024-05-12 11:07:34 +0200

    contenu

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,40 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      maxWidth: {
+        '8xl':'60rem',
+      },
+      margin: {
+        '30':'8rem'
+      },
+    backgroundColor: {
+      skin: {
+        background: 'var(--background)',
+        foreground: 'var(--foreground)'
+      }
+    },
+    colors: {
+      skin: {
+        muted: 'var(--muted)',
+        muted_foreground: 'var(--muted-foreground)',
+        popover: 'var(--popover)',
+        popover_foreground: 'var(--popover-foreground)',
+        card: 'var(--card)',
+        card_foreground: 'var(--card-foreground)',
+        border: 'var(--border)',
+        input: 'var(--input)',
+        primary: 'var(--primary)',
+        primary_foreground: 'var(--primary-foreground)',
+        secondary: 'var(--secondary)',
+        secondary_foreground: 'var(--secondary-foreground)',
+        accent: 'var(--accent)',
+        accent_foreground: 'var(--accent-foreground)',
+        destrutive_foreground: 'var(--destrutive-foreground)'
+      }
+    },
+    },
   },
   plugins: [],
 }
